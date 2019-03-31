e5cc8da6270f6c4a74733b8eb3bc6f6beb25d778
MatiasZunigaL <matias.zuniga.l@mail.pucv.cl>
2019-03-30 23:06:21 -0300

    Matias

--- src/cl/loccet/model/Constructora.java
+++ src/cl/loccet/model/Constructora.java
@@ -1,5 +1,11 @@
 package cl.loccet.model;
 
-public class Constructora {
+public class Constructora{
+    private String companyRun;
+    private String companyName;
 
+    public Constructora(String companyRun, String companyName) {
+        this.companyRun = companyRun;
+        this.companyName = companyName;
+    }
 }
