c9158d42d4c1c4719920a59ff69a53053d356fd9
MatiasZunigaL <matias.zuniga.l@mail.pucv.cl>
2019-03-31 01:14:51 -0300

    Creación variables de Inicio y fin de una obra.

--- src/cl/loccet/model/Proyecto.java
+++ src/cl/loccet/model/Proyecto.java
@@ -1,11 +1,15 @@
 package cl.loccet.model;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 
 public class Proyecto {
 
     private int id;
+    private Date fechaInicioProyecto;
+
+    private Date fechaTerminoProyecto;
 
     private double estimacion;
 
@@ -16,12 +20,30 @@ public class Proyecto {
     private HashMap<String, Trabajador> mapTrabajadores;
     //Implementar inventario.
 
-    public Proyecto(int id,double estimacion,double costoReal) {
+    public Proyecto(int id,double estimacion,double costoReal, Date fechaInicioProyecto, Date fechaTerminoProyecto) {
         this.id = id;
+        this.fechaInicioProyecto = fechaInicioProyecto;
+        this.fechaTerminoProyecto = fechaTerminoProyecto;
         this.estimacion = estimacion;
         this.costoReal = costoReal;
         listaTrabajadores = new ArrayList<>();
         mapTrabajadores = new HashMap<>();
+    }
+
+    public void setFechaInicioProyecto(Date fechaInicioProyecto) {
+        this.fechaInicioProyecto = fechaInicioProyecto;
+    }
+
+    public void setFechaTerminoProyecto(Date fechaTerminoProyecto) {
+        this.fechaTerminoProyecto = fechaTerminoProyecto;
+    }
+
+    public Date getFechaInicioProyecto() {
+        return fechaInicioProyecto;
+    }
+
+    public Date getFechaTerminoProyecto() {
+        return fechaTerminoProyecto;
     }
 
     public int getId() {
@@ -46,7 +68,7 @@ public class Proyecto {
     }
     public void mostrarTrabajadores(){
         for (int i = 0; i < listaTrabajadores.size(); i++){
-            //Mostrar por pantalla.****
+            //TODO: Mostrar por pantalla.****
         }
     }
 }
