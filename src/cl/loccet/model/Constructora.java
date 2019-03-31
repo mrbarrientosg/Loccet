12e6188a007081c0a31e4c3bb517577bf44a6274
MatiasZunigaL <matias.zuniga.l@mail.pucv.cl>
2019-03-30 23:06:21 -0300

    Matias

--- src/cl/loccet/model/Constructora.java
+++ src/cl/loccet/model/Constructora.java
@@ -1,29 +1,11 @@
 package cl.loccet.model;
 
-public class Constructora {
+public class Constructora{
+    private String companyRun;
+    private String companyName;
 
-    private String rut;
-
-    private String nombre;
-
-    public Constructora(String rut, String nombre) {
-        this.rut = rut;
-        this.nombre = nombre;
-    }
-
-    public void setRut(String rut) {
-        this.rut = rut;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public String getRut() {
-        return rut;
+    public Constructora(String companyRun, String companyName) {
+        this.companyRun = companyRun;
+        this.companyName = companyName;
     }
 }
