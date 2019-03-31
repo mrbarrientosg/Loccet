1deb61a8881202255df50740701c20d5c41ff360
MatiasZunigaL <matias.zuniga.l@mail.pucv.cl>
2019-03-30 23:38:41 -0300

    Clase constructora

--- src/cl/loccet/model/Constructora.java
+++ src/cl/loccet/model/Constructora.java
@@ -1,11 +1,50 @@
-package cl.loccet.model;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Constructora{
-    private String companyRun;
-    private String companyName;
+    private String rutEmpresa;
+    private String nombreEmpresa;
+    private  ArrayList<Obra> listaObras;
+    //TODO:permisos
+    private HashMap<Integer, Obra> mapObra;
 
-    public Constructora(String companyRun, String companyName) {
-        this.companyRun = companyRun;
-        this.companyName = companyName;
+    public Constructora(String rutEmpresa, String nombreEmpresa) {
+        this.rutEmpresa = rutEmpresa;
+        this.nombreEmpresa = nombreEmpresa;
+        listaObras = new ArrayList<Obra>();
+        mapObra = new HashMap<>();
+    }
+    //Setter
+
+    public void setRutEmpresa(String rutEmpresa) {
+        this.rutEmpresa = rutEmpresa;
+    }
+
+    public void setNombreEmpresa(String nombreEmpresa) {
+        this.nombreEmpresa = nombreEmpresa;
+    }
+
+    //Getter
+
+    public String getRutEmpresa() {
+        return rutEmpresa;
+    }
+
+    public String getNombreEmpresa() {
+        return nombreEmpresa;
+    }
+
+    //Metodos
+
+    public void agregarObra(Obra obra){
+        listaObras.add(obra);
+        mapObra.put(obra.getIdObra(),obra);
+    }
+
+    public boolean agregarTrabajador(Integer idObra, Trabajador trabajador){
+        if(mapObra.get(idObra) == null) return false;
+        mapObra.get(idObra).agregarTrabajador(trabajador);
+        return true;
     }
 }
+
