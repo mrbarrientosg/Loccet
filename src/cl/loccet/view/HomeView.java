afda65dfab3292dc64aeb4c303b75ddc8dbbd035
Matias Barrientos <matias.barrientosg@gmail.com>
2019-04-02 16:25:19 -0300

    Creacion del controlador y router de la vista home

--- src/cl/loccet/view/HomeView.java
+++ src/cl/loccet/view/HomeView.java
@@ -2,6 +2,7 @@ package cl.loccet.view;
 
 import cl.loccet.base.Injectable;
 import cl.loccet.base.View;
+import cl.loccet.controller.HomeController;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
@@ -10,11 +11,17 @@ public class HomeView extends View {
 
     private MenuBarView menuBarView = Injectable.getIntance().find(MenuBarView.class);
 
+    private HomeController controller;
+
     @FXML
     private BorderPane borderPane;
 
     @FXML
     private Button backButton;
+
+    public void setController(HomeController controller) {
+        this.controller = controller;
+    }
 
     public HomeView() {
         super("General");
