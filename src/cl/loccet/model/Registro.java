b305739b4f24b877f37914391116f8d911f3a6dc
Matias Barrientos <matias.barrientosg@gmail.com>
2019-04-10 21:23:25 -0400

    Nueva clas Reporte y Registro

---
+++ src/cl/loccet/model/Registro.java
@@ -0,0 +1,14 @@
+package cl.loccet.model;
+
+/**
+ * Clase que maneja el registro de un reporte
+ * que especialidad trabajo, cuantos y las horas
+ */
+public class Registro {
+
+    private String especialidad;
+
+    private int cantidadDeTrabajadores;
+
+    private double horasTrabajadas;
+}
