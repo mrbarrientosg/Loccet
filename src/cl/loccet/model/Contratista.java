3d23091f620c854e6dec69d1d7437ea8ce4b6ab5
Matias Barrientos <matias.barrientosg@gmail.com>
2019-04-10 23:42:52 -0400

    Nueva clase Contratista

---
+++ src/cl/loccet/model/Contratista.java
@@ -0,0 +1,19 @@
+package cl.loccet.model;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class Contratista {
+
+    private String rut;
+
+    private String nombre;
+
+    private Localizacion localizacion;
+
+    private String telefono;
+
+    private String correoElectronico;
+
+    private ArrayList<Trabajador> trabajadores;
+}
